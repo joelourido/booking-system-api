3ea52274b3f65ed39c81b40c839c5e4ef907fd69
joelourido <joemirlourido@outlook.com>
2025-11-11 23:54:33 +0900

    docs: update er-diagram

---
+++ db/schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE movie (
+	movie_id SERIAL PRIMARY KEY,
+	title TEXT,
+	release_date DATE,
+	synopsis TEXT,
+	duration INT,
+	img_url VARCHAR(255),
+	trailer_url VARCHAR(255)
+);
+
+CREATE TABLE room (
+	room_id INT PRIMARY KEY,
+	room_capacity INT NOT NULL
+);
+
+CREATE TABLE seat (
+	seat_id SERIAL PRIMARY KEY,
+	room_id INT REFERENCES room(room_id) ON DELETE CASCADE,
+	seat_number INT NOT NULL,
+	row VARCHAR(8) NOT NULL
+);
+
+CREATE TABLE session (
+	session_id SERIAL PRIMARY KEY,
+	movie_id INT REFERENCES movie(movie_id) ON DELETE CASCADE,
+	room_id INT REFERENCES room(room_id) ON DELETE CASCADE,
+	start_time TIMESTAMP NOT NULL,
+	end_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE app_user (
+	user_id SERIAL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE booking (
+	booking_id SERIAL PRIMARY KEY,
+	user_id INT REFERENCES app_user(user_id) ON DELETE CASCADE,
+	session_id INT REFERENCES session(session_id) ON DELETE CASCADE,
+	booking_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE booking_seat (
+	booking_id INT REFERENCES booking(booking_id) ON DELETE CASCADE,
+	seat_id INT REFERENCES seat(seat_id) ON DELETE CASCADE,
+	PRIMARY KEY (booking_id, seat_id)
+);
+
+CREATE TABLE admin (
+	admin_id SERIAL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password_hash VARCHAR(255) NOT NULL
+);
--- docs/ER-Diagram.md
+++ docs/ER-Diagram.md
@@ -1,98 +1,61 @@
+![ER Diagram](docs/ER-Diagram.svg)
+
+
 erDiagram
 
 MOVIE {
-
 int movie_id PK
-
 string title
-
 date release_date
-
 string synopsis
-
 int duration
-
 string img_url
-
 string trailer_url
-
 }
 
 ROOM {
-
 int room_id PK
-
 int room_capacity
-
 }
 
 SEAT {
-
 int seat_id PK
-
 int room_id FK
-
 int seat_number
-
 string row
-
 }
 
 SESSION {
-
 int session_id PK
-
 int movie_id FK
-
 int room_id FK
-
 datetime start_time
-
 datetime end_time
-
 }
 
 USER {
-
 int user_id PK
-
 string email UK
-
 string password_hash
-
 }
 
 BOOKING {
-
 int booking_id PK
-
 int user_id FK
-
 int session_id FK
-
 datetime booking_time
-
 }
 
 BOOKING_SEAT{
-
 int booking_id FK
-
 int seat_id FK
-
 }
 
 ADMIN {
-
 int admin_id PK
-
 string email UK
-
 string password_hash
-
-}
-
-  
+} 
 
 %%Relationships
 
